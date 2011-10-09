ac20447ee3d66b3edb8520c5ac88f10321a38ebd
Galen Collins <bashwork@gmail.com>
2011-10-09 14:15:00 -0700

    quick fix

--- shell/settings.py
+++ shell/settings.py
@@ -181,40 +181,40 @@ DEBUG_TOOLBAR_CONFIG = {
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 # --------------------------------------------------------------------------------
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'WARNING',
-        'handlers': ['sentry'],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-    },
-    'handlers': {
-        'sentry': {
-            'level': 'DEBUG',
-            'class': 'sentry.client.handlers.SentryHandler',
-            'formatter': 'verbose',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        }
-    },
-    'loggers': {
-        'sentry.errors': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        #'django.request': {
-        #    'handlers': ['mail_admins'],
-        #    'level': 'ERROR',
-        #    'propagate': True,
-        #},
-    }
-}
+#LOGGING = {
+#    'version': 1,
+#    'disable_existing_loggers': True,
+#    'root': {
+#        'level': 'WARNING',
+#        'handlers': ['sentry'],
+#    },
+#    'formatters': {
+#        'verbose': {
+#            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+#        },
+#    },
+#    'handlers': {
+#        'sentry': {
+#            'level': 'DEBUG',
+#            'class': 'sentry.client.handlers.SentryHandler',
+#            'formatter': 'verbose',
+#        },
+#        'console': {
+#            'level': 'DEBUG',
+#            'class': 'logging.StreamHandler',
+#            'formatter': 'verbose',
+#        }
+#    },
+#    'loggers': {
+#        'sentry.errors': {
+#            'handlers': ['console'],
+#            'level': 'DEBUG',
+#            'propagate': False,
+#        },
+#        #'django.request': {
+#        #    'handlers': ['mail_admins'],
+#        #    'level': 'ERROR',
+#        #    'propagate': True,
+#        #},
+#    }
+#}
