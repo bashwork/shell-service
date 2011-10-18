a8b3eb19532caffd92f5218cddddfadd81938fe1
Galen Collins <bashwork@gmail.com>
2011-10-17 19:43:52 -0700

    adding client

---
+++ shell/apps/serial/client.py
@@ -0,0 +1,13 @@
+import socket
+from simplejson import loads
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('radiant-meadow-2958.herokuapp.com', 8080))
+sock.send("GET / HTTP/1.1\r\n\r\n")
+data = sock.recv(1024)
+print data
+
+while len(data):
+      data = sock.recv(1024).split('\r\n')[1]
+      print loads(data)
+sock.close()
