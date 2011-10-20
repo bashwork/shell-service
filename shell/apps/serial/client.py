5b6fb7858811823003652c23f1bec414c77eaa97
Galen Collins <bashwork@gmail.com>
2011-10-19 18:56:29 -0700

    updating client

--- shell/apps/serial/client.py
+++ shell/apps/serial/client.py
@@ -1,10 +1,11 @@
 import socket
 from simplejson import loads
 
+request = "GET / HTTP/1.1\r\nHOST: radiant-meadow-2958.herokuapp.com\r\n\r\n"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('localhost', 8080))
-#sock.connect(('radiant-meadow-2958.herokuapp.com', 8080))
-sock.send("GET / HTTP/1.1\r\n\r\n")
+#sock.connect(('localhost', 8080))
+sock.connect(('radiant-meadow-2958.herokuapp.com', 80))
+sock.send(request)
 data = sock.recv(1024)
 print data
 
