0911be32848f650380eeea646d3ba5cb64216149
Galen Collins <bashwork@gmail.com>
2011-11-26 20:21:47 -0800

    finished updating ui for now

--- shell/apps/frontend/static/js/application.js
+++ shell/apps/frontend/static/js/application.js
@@ -1,38 +1,63 @@
+/**
+ * API wrapper for the shell platform
+ * - TODO convert to template and generate development/production
+ */
 (function($) {
   var common = {
-    base : "/api/v1/",
+    api : {
+      base : "/api/v1/"
+    },
     //base : "http://radiant-meadow-2958.herokuapp.com/api/v1/",
+    template : {
+      base : '/template/',
+      compiled : {}
+    }
   };
 
   $.shell = {};
+  $.shell.template = {
+    initialize : function(name) {
+      name = name.replace('.', '/');
+      $.get(common.template.base + name + '/', function(result) {
+        common.template.compiled[name] = _.template(result);
+      });
+    },
+    compile : function(name, context) {
+      name = name.replace('.', '/');
+      return common.template.compiled[name](context);
+    }
+  };
   $.shell.player = {
     all : function() {
-      return $.getJSON(common.base + "player/?callback=?");
+      return $.getJSON(common.api.base + "player/?callback=?");
     },
     get : function(id) {
-      return $.getJSON(common.base + "player/" + id + "/?callback=?");
-    },
+      return $.getJSON(common.api.base + "player/" + id + "/?callback=?");
+    }
   };
   $.shell.history = {
     get : function(id) {
-      return $.getJSON(common.base + "history/" + id + "/?callback=?");
+      return $.getJSON(common.api.base + "history/" + id + "/?callback=?");
     },
     player : function(id) {
-      return $.getJSON(common.base + "player/" + id + "/history/?callback=?");
-    },
+      return $.getJSON(common.api.base + "player/" + id + "/history/?callback=?");
+    }
   };
   $.shell.trauma = {
     get : function(id) {
-      return $.getJSON(common.base + "trauma/" + id + "/?callback=?");
+      return $.getJSON(common.api.base + "trauma/" + id + "/?callback=?");
     },
     player : function(id) {
-      return $.getJSON(common.base + "player/" + id + "/trauma/?callback=?");
-    },
+      return $.getJSON(common.api.base + "player/" + id + "/trauma/?callback=?");
+    }
   };
 })(jQuery);
 
 (function($) {
   Application = { };
+  /**
+   * filter view of current players on main page
+   */
   Application.filter = function(value) {
     // todo split value and check if any in following
     $('a.player').each(function(index) {
@@ -45,6 +70,9 @@
     });
   };
 
+  /**
+   * Toggle player information chart
+   */
   Application.chart  = function(history) {
     var chart = new Highcharts.Chart({
       chart : {
@@ -78,15 +106,22 @@
     });
   };
 
+  /**
+   * Toggle player information view
+   */
   Application.select = function(event) {
     $.shell.history.player(event.data.player.id).then(Application.chart);
+    $('#player-information').html(
+      $.shell.template.compile('player.information', event.data));
     $('#player-dialog').dialog('option', 'title',
       event.data.player.firstname + ' ' + event.data.player.lastname);
     $('#player-dialog').dialog('open');
-    // todo, covert this to template
   };
 
   Application.initialize = function() {
+    /**
+     * Initialize information dialog
+     */
     $('#player-dialog').dialog({
       autoOpen      : false,
       closeOnEscape : true,
@@ -99,6 +134,11 @@
       zIndex        : 10000
     });
 
+    /**
+     * Initialize players for the front page:
+     * - auto suggest initialize
+     * - main helmet view initialize
+     */
     $.shell.player.all().then(function(players) {
       $.each(players, function(i, player) {
         $("<img/>", {
@@ -125,6 +165,12 @@
       }).autocomplete({ source : names });
     });
 
+    /**
+     * Initialize templates for front page
+     */
+    _.templateSettings = { interpolate : /\{\{(.+?)\}\}/g };
+    $.shell.template.initialize('player.information');
+    $.shell.template.initialize('contact.information');
   };
 
 })(jQuery);
